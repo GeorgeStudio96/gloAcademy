e3521281e62be75d1aa38a6dab84807ac671a16e
George Ershov <geo.erh96@gmail.com>
2022-12-02 10:19:08 +0300

    домашка lesson01

--- js/app.js
+++ js/app.js
@@ -1,6 +1,11 @@
-    // title,
-    // screens,
-    // screenPrice,
-    // rollback,
-    // fullPrice,
-    // adaptive
+// homework lesson 01
+
+let title;
+let screens;
+let screenPrice;
+let rollback;
+let fullPrice;
+let adaptiv;
+
+alert('homework lesson01')
+console.log('homework lesson01')
