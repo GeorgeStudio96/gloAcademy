5e76664290468ab3a84350d38a375980e543ee8d
George Ershov <geo.erh96@gmail.com>
2022-12-02 11:25:50 +0300

    homework lesson02

--- js/app.js
+++ js/app.js
@@ -5,7 +5,50 @@ let screens;
 let screenPrice;
 let rollback;
 let fullPrice;
-let adaptiv;
+let adaptive;
 
 alert('homework lesson01')
 console.log('homework lesson01')
+
+
+
+// homework lesson 2
+
+// Следующим переменным присвоить значения
+
+title = 'Lesson 01';
+screens = 'Простые, Сложные, Интерактивные';
+screenPrice = 10;
+rollback = 76;
+fullPrice = 1000000;
+adaptive = true
+
+// Вывести в консоль тип данных значений переменных title, fullPrice, adaptive;
+console.log(typeof title)
+console.log(typeof fullPrice)
+console.log(typeof adaptive)
+
+// Вывести в консоль длину строки из переменной screens
+
+console.log(screens.length)
+
+// Вывести в консоль “Стоимость верстки экранов (screenPrice) рублей/ долларов/гривен/юани” 
+// и “Стоимость разработки сайта (fullPrice) рублей/ долларов/гривен/юани”
+
+console.log((screenPrice + ' Рублей'))
+console.log((screenPrice + ' Долларов'))
+console.log((fullPrice + ' Рублей'))
+console.log((fullPrice + ' Долларов'))
+
+// Привести строку screens к нижнему регистру и разбить строку на массив, вывести массив в консоль
+
+let screensToLowerCase = screens.toLowerCase();
+console.log(screensToLowerCase)
+
+let array = screensToLowerCase.split(', ')
+console.log(array)
+
+// Вывести в консоль Процент отката посреднику за работу (fullPrice * (rollback/100))
+
+console.log((fullPrice * (rollback / 100)))
+
---
+++ my-files/test.js
@@ -0,0 +1,11 @@
+let text = 'Hello, world'
+
+console.log(text.charAt(3))
+console.log(text[0])
+console.log(text.substring(1, 10))
+console.log(text.slice(-8))
+console.log(text.indexOf('world'))
+console.log(text.includes('world'))
+// console.log(text.replace('Hello', 'Bye'))
+
+console.log(text.split(', '))
