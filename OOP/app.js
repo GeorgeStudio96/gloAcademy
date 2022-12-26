d970f9c188dbfbd37cce8148e11451c0dea65f3e
George Ershov <geo.erh96@gmail.com>
2022-12-26 09:53:46 +0300

    prompt added

--- OOP/app.js
+++ OOP/app.js
@@ -4,22 +4,25 @@
 document.addEventListener("DOMContentLoaded", () => {
 
 
-    const DomElement = function (selector, height, width, bg, fontSize) {
+    const DomElement = function (selector, height, width, bg, fontSize, text) {
         this.selector = selector
         this.height = height
         this.width = width
         this.bg = bg
         this.fontSize = fontSize
+        this.text = text
 
     }
 
 
     DomElement.prototype.createElement = function () {
+        this.text = prompt('Введите текст')
+        console.log(this.text);
 
         if (this.selector.startsWith('.')) {
             const newElem = document.createElement('div')
             newElem.classList.add('block')
-            newElem.innerHTML = 'Text'
+            newElem.innerHTML = this.text
             newElem.style.cssText = `font-size: ${this.fontSize}; 
                     background: ${this.bg}; color: white; 
                     width: ${this.width}; height: ${this.height}; display: flex; align-items: center;
@@ -45,7 +48,7 @@ document.addEventListener("DOMContentLoaded", () => {
         } else if (this.selector.startsWith('#')) {
             const newElem = document.createElement('p')
             newElem.setAttribute('id', 'best')
-            newElem.innerHTML = 'TextTextTextTextTextTextTextTextTextTextText'
+            newElem.innerHTML = this.text
             newElem.style.cssText = `font-size: ${this.fontSize}; color: black`
             console.log(newElem);
             document.body.append(newElem);
@@ -58,7 +61,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 
-    const elem = new DomElement('.text', '300px', '300px', 'black', '18px')
+    const elem = new DomElement('.text', '300px', '300px', 'black', '18px', this.text)
 
     elem.createElement()
 
